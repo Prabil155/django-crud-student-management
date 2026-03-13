f601fe3282c787560bbad186be6d6afea25632a2
Prabil155 <prabil22timalsena@gmail.com>
2026-03-13 15:54:10 +0545

    Initial Django CRUD project

---
+++ students/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Student
+
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/list.html', {'students': students})
+
+
+def add_student(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        course = request.POST['course']
+
+        Student.objects.create(name=name, email=email, course=course)
+        return redirect('/')
+
+    return render(request, 'students/add.html')
+
+
+def delete_student(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect('/')
